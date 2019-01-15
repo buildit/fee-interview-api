976e60d66501084703f01165d7ff8955dad421bb
Ben Hernandez <benaychh@gmail.com>
2019-01-15 14:25:45 -0600

    initial setup

---
+++ api/text.db.js
@@ -0,0 +1,150 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What was the original name of Constantinople?',
+  },
+  {
+    id: 2,
+    title: 'What is the name for the mapped hexagonal geographic areas that make up the cellular phone grid?',
+  },
+  {
+    id: 3,
+    title: 'The last letter of the Greek alphabet is',
+  },
+  {
+    id: 4,
+    title: 'What is the name for a computer pointing device?',
+  },
+  {
+    id: 5,
+    title: 'In the film Pan’s Labyrinth, what is the little girl named?',
+  },
+];
+
+const answers = [
+  {
+    id: 1,
+    questionId: 1,
+    title: 'Byzantium',
+    isCorrect: true,
+  },
+  {
+    id: 2,
+    questionId: 1,
+    title: 'Omega',
+    isCorrect: false,
+  },
+  {
+    id: 3,
+    questionId: 1,
+    title: 'France',
+    isCorrect: false,
+  },
+  {
+    id: 4,
+    questionId: 1,
+    title: 'Knee',
+    isCorrect: false,
+  },
+  {
+    id: 5,
+    questionId: 2,
+    title: 'Cells',
+    isCorrect: true,
+  },
+  {
+    id: 6,
+    questionId: 2,
+    title: 'Omega',
+    isCorrect: false,
+  },
+  {
+    id: 7,
+    questionId: 2,
+    title: 'Byzantium',
+    isCorrect: false,
+  },
+  {
+    id: 8,
+    questionId: 2,
+    title: 'Knee',
+    isCorrect: false,
+  },
+  {
+    id: 9,
+    questionId: 3,
+    title: 'Omega',
+    isCorrect: true,
+  },
+  {
+    id: 10,
+    questionId: 3,
+    title: 'Byzantium',
+    isCorrect: false,
+  },
+  {
+    id: 11,
+    questionId: 3,
+    title: 'France',
+    isCorrect: false,
+  },
+  {
+    id: 12,
+    questionId: 3,
+    title: 'Knee',
+    isCorrect: false,
+  },
+  {
+    id: 13,
+    questionId: 4,
+    title: 'A mouse',
+    isCorrect: true,
+  },
+  {
+    id: 14,
+    questionId: 4,
+    title: 'A rat',
+    isCorrect: false,
+  },
+  {
+    id: 15,
+    questionId: 4,
+    title: 'A rabbit',
+    isCorrect: false,
+  },
+  {
+    id: 16,
+    questionId: 4,
+    title: 'An Elephant',
+    isCorrect: false,
+  },
+  {
+    id: 17,
+    questionId: 5,
+    title: 'Ofelia',
+    isCorrect: true,
+  },
+  {
+    id: 18,
+    questionId: 5,
+    title: 'Anna',
+    isCorrect: false,
+  },
+  {
+    id: 19,
+    questionId: 5,
+    title: 'Ellen',
+    isCorrect: false,
+  },
+  {
+    id: 20,
+    questionId: 5,
+    title: 'Nancy',
+    isCorrect: false,
+  },
+];
+
+module.exports = {
+  answers,
+  questions,
+};
